f81ccc16c400da8f454f7789f81f60733552995f
= <=>
2025-09-23 21:27:04 -0400

    Optimized MainActivity.kt imports

--- app/src/main/java/com/vongda/netbuddy/MainActivity.kt
+++ app/src/main/java/com/vongda/netbuddy/MainActivity.kt
@@ -8,17 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.vongda.handgestureprototype.ui.handgesture.PoseCameraScreen
-import com.vongda.netbuddy.data.viewmodels.MatchViewModel
-import com.vongda.netbuddy.ui.home.HomeScreen
-import com.vongda.netbuddy.ui.instructions.InstructionsScreen
-import com.vongda.netbuddy.ui.instructions.components.Instruction
-import com.vongda.netbuddy.ui.matchsettings.MatchSettingsScreen
-import com.vongda.netbuddy.ui.results.ResultsScreen
 import com.vongda.netbuddy.ui.theme.PoseDetectorTheme
 
 class MainActivity : ComponentActivity() {
