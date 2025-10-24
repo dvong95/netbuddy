2f58bf34ba2cd01a0c31facb2ad896d5e676f3b4
Sam Verdejo <sjverdejo@gmail.com>
2025-10-24 17:00:56 -0400

    updated results page

--- app/src/main/java/com/vongda/netbuddy/ui/components/MainButton.kt
+++ app/src/main/java/com/vongda/netbuddy/ui/components/MainButton.kt
@@ -1,10 +1,12 @@
 package com.vongda.netbuddy.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,16 +20,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun MainButton (text: String, padding: Boolean = false, onClick: () -> Unit) {
+fun MainButton (text: String, padding: Boolean = false, secondaryTheme: Boolean = false, onClick: () -> Unit) {
     Button(
         onClick = onClick,
         modifier = Modifier.fillMaxWidth().padding(28.dp)
             .shadow(
                 elevation = 4.dp,
             ),
+        border = if (secondaryTheme) BorderStroke(1.dp, Color.White) else null,
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.White,
-            contentColor = Color.Black
+            containerColor = if (secondaryTheme) Color(0xFF5752AD) else Color.White,
+            contentColor = if (secondaryTheme) Color.White else Color.Black,
+
         ),
         shape = RoundedCornerShape(4.dp)
     ){
@@ -36,7 +40,7 @@ fun MainButton (text: String, padding: Boolean = false, onClick: () -> Unit) {
             modifier = Modifier.padding(8.dp),
             style = TextStyle(
                 fontSize = 18.sp,
-                color = Color.Black,
+                color = if (secondaryTheme) Color.White else Color.Black,
                 shadow = Shadow(
                     color = Color.Black.copy(alpha = 0.4f),
                     offset = Offset(0.5f, 0.5f),
